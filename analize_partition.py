e9fffc754f7b815def8caaff415633fe4cb9b5ed
deliaMitric <126279438+deliaMitric@users.noreply.github.com>
2023-12-18 15:47:04 +0200

    recursive/first level partition traversal

--- analize_partition.py
+++ analize_partition.py
@@ -17,35 +17,40 @@ def build_partition_path(partition):
     return partition_path
 
 def make_plots_number_size(extensions, number_files):
-    #Deoarece in extensions sunt extensii ce corespund la foarte putine fisiere
-    #vom lua in considerare doar procentele >= 0.35 pentru a nu incarca diagrama
-    new_extensions = [ext for ext in extensions if ((len(extensions[ext]) / number_files) * 100) >= 0.35]
-    percents_number = [((len(extensions[ext]) / number_files) * 100) for ext in new_extensions]
+    try:
+        # Deoarece in extensions sunt extensii ce corespund la foarte putine fisiere
+        # vom lua in considerare doar procentele >= 0.35 pentru a nu incarca diagrama
+        new_extensions = [ext for ext in extensions if ((len(extensions[ext]) / number_files) * 100) >= 0.35]
+        percents_number = [((len(extensions[ext]) / number_files) * 100) for ext in new_extensions]
 
-    figure1, ax1 = plot.subplots(figsize=(25, 8))
+        figure1, ax1 = plot.subplots(figsize=(25, 8))
 
-    #Realizarea bar chart ului
-    ax1.bar(new_extensions, percents_number, color='green')
-    ax1.set_xticks(range(len(new_extensions)))
-    ax1.set_xticklabels(new_extensions, rotation=45, ha='right')
-    ax1.set_xlabel("Extensii:")
-    ax1.set_ylabel("Procente:")
-    ax1.set_title("Proportia fiecarui tip ca numar:")
+        #Realizarea bar chart ului
+        ax1.bar(new_extensions, percents_number, color='green')
+        ax1.set_xticks(range(len(new_extensions)))
+        ax1.set_xticklabels(new_extensions, rotation=45, ha='right')
+        ax1.set_xlabel("Extensii:")
+        ax1.set_ylabel("Procente:")
+        ax1.set_title("Proportia fiecarui tip ca numar:")
 
-    sizes = [sum(extensions[ext]) for ext in extensions]
-    total_size = sum(sizes)
+        sizes = [sum(extensions[ext]) for ext in extensions]
+        total_size = sum(sizes)
 
-    new_extensions = [ext for ext in extensions if ((sum(extensions[ext]) / total_size) * 100) >= 1]
-    percents_size = [((sum(extensions[ext]) / total_size) * 100) for ext in new_extensions]
 
-    figure, ax2 = plot.subplots(figsize=(25, 8))
+        new_extensions = [ext for ext in extensions if ((sum(extensions[ext]) / total_size) * 100) >= 1]
+        percents_size = [((sum(extensions[ext]) / total_size) * 100) for ext in new_extensions]
 
-    # Realizarea pie chart ului
-    ax2.pie(percents_size, labels=new_extensions, autopct="%1.1f%%", startangle=90)
-    ax2.axis("equal")
-    ax2.set_title("Proportia fiecarui tip ca size:")
+        figure, ax2 = plot.subplots(figsize=(25, 8))
 
-    plot.show()
+        # Realizarea pie chart ului
+        ax2.pie(percents_size, labels=new_extensions, autopct="%1.1f%%", startangle=90)
+        ax2.axis("equal")
+        ax2.set_title("Proportia fiecarui tip ca size:")
+
+        plot.show()
+
+    except ZeroDivisionError as e:
+        print("Partitia nu contine fisiere.")
 
 #Parcurgerea recursiva a partitiei-----------------------------------------------------
 def analyze_rec(partition):
@@ -145,7 +150,99 @@ def analyze_rec(partition):
 
 
 #Parcurgerea doar a primului nivel-----------------------------------------------------
+def analyze_first_level(partition):
+    number_of_files = 0
+    number_of_dirs = 0
+    extensions = {}
 
+    try:
+        partition_path = build_partition_path(partition)
+
+        if not os.path.exists(partition_path):
+            raise Exception("Partitia data nu exista")
+
+        if os.access(partition_path, os.R_OK):
+            try:
+                for element in tqdm(os.listdir(partition_path), desc="Progress"):
+
+                    #construimk path-ul elementului
+                    elem_path = os.path.join(partition_path, element)
+
+                    #verificam daca emlement este file sau director
+                    if os.path.isdir(elem_path):
+                        number_of_dirs +=1
+
+                    elif os.path.isfile(elem_path):
+                        number_of_files +=1
+                        file_size = os.path.getsize(elem_path)
+
+                        #Extragem extensia fisierului
+                        file_ext = os.path.splitext(element)[1]
+
+                        #verificam daca fisierul nu e standard, adica contine o insiruire de valori precum version=.., culture-.. etc.
+                        if "," in file_ext and "." in file_ext:
+                            if "non_standard" not in extensions:
+                                extensions["non_standard"] = [file_size]
+                            else:
+                                extensions["non_standard"].append(file_size)
+                        # verificam daca extensia e un numar
+                        elif file_ext[1:].isdigit():
+                            if "number" not in extensions:
+                                extensions["number"] = [file_size]
+                            else:
+                                extensions["number"].append(file_size)
+
+                        # verificam daca fisierul nu are extensie
+                        elif file_ext == "":
+                            if "no_ext" not in extensions:
+                                extensions["no_ext"] = [file_size]
+                            else:
+                                extensions["no_ext"].append(file_size)
+
+                        else:
+                            if sys.platform == "win64" or sys.platform == "win32":
+                                file_ext = file_ext.lower()
+
+                            if file_ext not in extensions:
+                                extensions[file_ext] = [file_size]
+                            else:
+                                extensions[file_ext].append(file_size)
+
+            #Daca vreun director e inaccesibil, lasam utilizatorul sa aleaga daca, continuam sau nu
+            except PermissionError as p:
+                print(f"Un fisier sau un director din partitia {partition}nu este accesibil.\nDoriti sa continuam prin excluderea acestuia? y/n")
+                response = input()
+                if response == "y" or response == "Y":
+                    pass
+                else:
+                    raise SystemExit
+            except Exception as e:
+                print(
+                    f"Am intampinat o eroare la deschiderea de director/fisier.\nDoriti sa continuam? y/n")
+                response = input()
+                if response == "y" or response == "Y":
+                    pass
+                else:
+                    raise SystemExit
+
+    #Cazul in care partitia nu e accesibila
+    except PermissionError as p:
+        print("Partitia data nu e accesibila!")
+        raise SystemExit
+
+    # Cazul in care partitia data nu se gaseste sau cazuri neasteptate
+    except Exception as e:
+        if len(e.args) > 0:
+            print(e.args[0])
+        else:
+            print(f"Eroare: {e}")
+        raise SystemExit
+    else:
+        print(f"Numar dir: {number_of_dirs}")
+        print(f"Numar files: {number_of_files}")
+        print(f"Numar extensii: {len(extensions)}")
+
+        make_plots_number_size(extensions, number_of_files)
 
 if __name__ == '__main__':
         if len(sys.argv) != 2:
@@ -161,7 +258,7 @@ if __name__ == '__main__':
                     analyze_rec(partition)
                     break
                 elif response.lower() == "n":
-                    #analyze_first_level(partition)
+                    analyze_first_level(partition)
                     break
                 else:
                     print("Va rog introduceti  R  pentru parcurgere RECURSIV sau  N  pentru parcurgerea PRIMUL NIVEL")
